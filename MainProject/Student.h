52a6785dfd7ac29b78dfdcf2ce5d9385ae33d35b
Timur Vasilevskiy <timyrvasilevskiy@gmail.com>
2025-04-09 18:00:39 +0300

    Constructors and distructor

--- MainProject/Student.h
+++ MainProject/Student.h
@@ -1,12 +1,60 @@
 #include "main.h"
 class Student {
+
+
 public:
+
 	string name;
 	int age;
 	double mark;
 	bool alive;
+	// default-constructor or constructor without arguments
+	Student() {
+		cout << "Default-constructor..." << endl;
+		name = "no name";
+		age = 13;
+		mark = 4.0;
+		alive = true;
+	}
+	//  constructor with arguments
+	Student(string nm) {
+		cout << "constructor with arguments" << endl;
+		name = nm;
+		age = 13;
+		mark = 4.0;
+		alive = true;
+	}
+	// constructor with arguments
+	Student(string nm, int a) {
+		cout << "constructor with arguments..." << endl;
+		name = nm;
+		age = a;
+		mark = 4.0;
+		alive = true;
+	}
+	//canonical-constructor
+	Student(string nm, int a, double m, bool al) {
+		cout << "canonical-constructor" << endl;
+		name = nm;
+		age = a;
+		mark = m;
+		alive = al;
+	}
+	// destructor
+	~Student() {
+		cout << "destructor..." << endl;
+	   }
+	//copy-constructor
+	Student( const Student& student) {
+		cout << "copy-constructor" << endl;
+		name = student.name;
+		age = student.age;
+		mark = student.mark;
+		alive = student.alive;
+	}
 
-	string getString() {
+
+	string toString() {
 		string s = "Name: " + name;
 		s += ", age: " + to_string(age);
 		s += ", mark: " + to_string(mark);
--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -1,52 +1,30 @@
 #include "Teacher.h"
 
-//void show(Student student) {
-//	cout << "Name: " << student.name << endl;
-//	cout << "Age: " << student.age << endl;
-//	cout << "Mark: " << student.mark << endl;
-//	cout << "Alive: " << (student.alive ? "yes" : "no") << endl;
-//
-//	cout << "----------" << endl;
-//}
+void test() {
+
+}
+void test(int n) {
+
+}
+void test(string n) {
+
+}
 
 int main() {
-	Student* list = nullptr;
-	int size;
+	Student student1{};
+	/*Student student1{};
+	Student student2("Alex");
+	Student student3("Vlad", 13, 10, true);
+	Student student4(student3);*/
+	Student student2("Alex");
+	//Student student3("Vlad", 13, 10, true);
+	Student student3("Vlad", 13);
+	Student student4(student3);
 
-	cout << "Input number of students: ";
-	cin >> size;
-
-	list = new Student[size];
-
-	cout << "Input data of students: ";
-
-	for (int i = 0; i < size; i++)
-	{
-		cout << "Student number " << i + 1 << ":\n";
-		cout << "name: ";
-		cin >> list[i].name;
-
-		cout << "age: ";
-		cin >> list[i].age;
-
-		cout << "mark: ";
-		cin >> list[i].mark;
-
-		cout << "is alive(y/n): ";
-		char answer;
-		cin >> answer;
-
-		list[i].alive = islower(answer) == 'y';
-	}
-
-	Teacher teacher;
-
-	Student result = teacher.getAllBestStudents(list, size);
-
-	string msg = result.alive ? "Best student is " +  result.name
-		: "No best students ";
-	
-	cout << msg;
+	cout << student1.toString() << endl;
+	cout << student2.toString() << endl;
+	cout << student3.toString() << endl;
+	cout << student4.toString() << endl;
 
 	return 0;
 }
