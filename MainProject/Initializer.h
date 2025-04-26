f00bfaa02150d461cb713325279ae621370eee83
Timur Vasilevskiy <timyrvasilevskiy@gmail.com>
2025-04-26 12:12:46 +0300

    Divide interface and implementation of classes

--- MainProject/Initializer.h
+++ MainProject/Initializer.h
@@ -2,31 +2,5 @@
 
 class Initializer {
 public:
-	void init(Student*& list, int count) {
-		if (list == nullptr && count > 0) {
-			list = new Student[count];
-		}
-
-		string names[]{ "Anna", "Alex", "Vlad" , "Matvey" ,
-			"Ivan" , "Timur", "Timofey", "Nikita", "Vladimir",
-			"Bogdan", "Rodion", "Alexey", "Daniil",
-			"Victor", "Kate", "Alice", "Maks" };
-
-		int minMark = 4;
-		int maxMark = 10;
-
-		int minAge = 13;
-		int maxAge = 18;
-
-		for (int i = 0; i < count; i++)
-		{
-			list[i].setName(names[rand() % 17]);
-			list[i].setAge(rand() % (maxAge - minAge + 1) + minAge);
-			for (int j = 0; j < list[i].getCountMark(); j++)
-			{
-				//list[i].getMarks()[j] = rand() % (maxMark - minMark + 1) + minMark;
-				*(list[i].getMarks() + j) = rand() % (maxMark - minMark + 1) + minMark;
-			}
-		}
-	}
+	void init(Student*& list, int count);
 };
--- MainProject/Student.h
+++ MainProject/Student.h
@@ -8,103 +8,27 @@ private:
 	int* marks;
 	bool alive;
 
+	string getAllMarks();
+
 public:
-	string getName() { return name; }
-	void setName(string name) { this->name = name; }
-
-	int getAge() { return age; }
-	void setAge(int age) {
-		if (age >= 13 && age <= 18) {
-			this->age = age;
-		}
-	}
-
-	int getCountMark() { return countMark; }
-
-	int* getMarks() {
-		return marks;
-	}
-
-	bool isAlive() { return alive; }
-	void setAlive(bool alive) { this->alive = alive; }
-
-	// default-constructor or constructor without argumnets
-	Student() : Student("no name", 13, 10, true) {
-		//cout << "Default-constructor..." << endl;		
-	}
-
-	// constructor with arguments
-	Student(string name) : Student(name, 13, 0, true) {
-		//cout << "constructor with arguments..." << endl;
-
-	}
-
-	// constructor with arguments
-	Student(string nm, int a) : Student(nm, a, 0, true) {
-		//cout << "constructor with arguments..." << endl;
-	}
-
-	// canonical-constructor
-	Student(string name, int age, int countMark, bool alive) {
-		//cout << "canonical-constructor..." << endl;
-		this->name = name;
-		this->age = age;
-		this->alive = alive;
-		this->countMark = countMark;
-		marks = new int[countMark];
-		for (int i = 0; i < countMark; i++)
-		{
-			marks[i] = 4;
-		}
-	}
-
-	// copy-constructor
+	Student() : Student("no name", 13, 10, true) {}
+	Student(string name) : Student(name, 13, 0, true) {}
+	Student(string nm, int a) : Student(nm, a, 0, true) {}
+	Student(string name, int age, int countMark, bool alive);
 	Student(const Student& student) : Student(student.name, student.age,
-		student.countMark, student.alive) {
-		//cout << "copy-constructor..." << endl;
-	}
+		student.countMark, student.alive) {}
+	~Student();
 
-	// destructor
-	~Student() {
-		//cout << "destructor..." << endl;
+	string getName();
+	void setName(string name);
+	int getAge();
+	void setAge(int age);
+	int getCountMark();
+	int* getMarks();
+	bool isAlive();
+	void setAlive(bool alive);
+	int getMark(int index);
+	void setMark(int index, int mark);
 
-		if (marks != nullptr) {
-			delete[] marks;
-		}
-	}
-
-	string toString() {
-		string s = "Name: " + this->name;
-		s += ", age: " + to_string(this->age);
-		s += ", marks: " + getAllMarks();
-		s += ", alive: ";
-		s += (this->alive ? "yes" : "no");
-		return s;
-	}
-
-	string getAllMarks() {
-		if (countMark == 0) {
-			return "[]";
-		}
-
-		string s = "";
-
-		for (int i = 0; i < countMark; i++)
-		{
-			s += to_string(marks[i]) + " ";
-		}
-
-		return s;
-	}
-
-	int getMark(int index) {
-		return index < 0 || index >= countMark || !marks ? 0 : marks[index];
-	}
-
-	void setMark(int index, int mark) {
-		if (index >= 0 && index < countMark && marks) {
-			marks[index] = mark;
-		}
-	}
-
+	string toString();
 };
